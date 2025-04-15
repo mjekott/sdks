7bb3d2dc33797b1c2e395086011de9a66ba52f7d
Mfonobong Ekott <ekottmfon@yahoo.com>
2025-04-16 00:30:34 +0100

    add euro

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,34 +1,31 @@
 import { StyleSheet, View } from 'react-native';
-/* import {
-  FlickPoundCollection,
-  FlickProvider,
-} from 'react-native-flick-pound-collection'; */
-
+import { Flick, FlickProvider } from 'react-native-flick-react-native-sdk';
 export default function App() {
-  /*  const handleError = (error: any) => {
+  const handleError = (error: any) => {
     console.log(error);
   };
 
   const handleSuccess = (data: any) => {
     console.log(data);
   };
- */
+
   return (
     <View style={styles.container}>
-      {/*   <FlickProvider
+      <FlickProvider
         environment="production"
         apiKey="U2FsdGVkX1/zDxxcuZW3y3g5DOMykj7Sr4CECH8tbw1Jdzta1L0ISTYvzu0xPCbRpj8XaKbD5DUrEycuKWWXa6QcVXGygtrxe3ARt+xH68NXLeNylwyqzgQ2Mlu0ZKQf78rB49yWyqefevQ+HAET6+/UOCd+RDfzvADVtm1nw7KnfyiGA4gkrxtgTrIEEsP7/vxTs/Ir1rTBMcQDRVKcRQ=="
         onError={handleError}
         onSuccess={handleSuccess}
       >
-        <FlickPoundCollection
+        <Flick
           config={{
-            amount: 500,
-            currency: 'GBP',
+            amount: 1000,
+            currency: 'EUR',
             cust_email: 'ekottmfon@gmail.com',
+            country: 'GB',
           }}
         />
-      </FlickProvider> */}
+      </FlickProvider>
     </View>
   );
 }
